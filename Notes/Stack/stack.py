d4137ee8ec87c69aae01a7d7380c9927a9a2f1cc
anubhav11803451 <anubhav.11803451@lpu.in>
2020-05-30 10:56:27 +0530

    Update stack.py

--- Notes/Stack/stack.py
+++ Notes/Stack/stack.py
@@ -1,32 +1,33 @@
 class Stack():
-    def __init__(self): # Constructor to initiate the object.
+    def __init__(self):  # Constructor to initiate the object.
         self.items = []
 
-    def push(self, items): # To Push the item in the empty stack list.
+    def push(self, items):  # To Push the item in the empty stack list.
         self.items.append(items)
 
-    def pop(self): # To Pop the last item pushed in stack list.
+    def pop(self):  # To Pop the last item pushed in stack list.
         return self.items.pop()
 
-    def get_stack(self): # To Fetch the items in stack list.
+    def get_stack(self):  # To Fetch the items in stack list.
         return self.items
 
-    def is_empty(self): # To check is stack list is empty.
+    def is_empty(self):  # To check is stack list is empty.
         return self.items == []
 
-    def peek(self): # To Fetch the last item in stack list.
+    def peek(self):  # To Fetch the last item in stack list.
         if not self.is_empty():
             return self.items[-1]
 
-objectStack = Stack() # Object of the Stack Class.
 
-print(objectStack.is_empty()) 
+objectStack = Stack()  # Object of the Stack Class.
+
+print(objectStack.is_empty())
 
 objectStack.push("Rat")
 objectStack.push("Cat")
 
 print(objectStack.get_stack())
- 
+
 objectStack.push("Bat")
 
 print(objectStack.get_stack())
