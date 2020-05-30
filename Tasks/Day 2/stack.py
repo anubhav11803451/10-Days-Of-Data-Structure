d973edf37aeb7ac92b789f920f494d56501d37ad
anubhav11803451 <anubhav.11803451@lpu.in>
2020-05-30 09:52:42 +0530

    Update stack.py

--- Tasks/Day 2/stack.py
+++ Tasks/Day 2/stack.py
@@ -10,7 +10,7 @@ class Node:
     def get_link_node(self):
         return self.link_node
 
-    def set_link_node(self, link_node):
+    def set_next_node(self, link_node):
         self.link_node=link_node
 
 class Stack(Node):
@@ -24,8 +24,8 @@ class Stack(Node):
 
     def push(self, value):
         if self.has_space():
-            super.__init__(value)
-            self.set_link_node(self.top_item)
+            super().__init__(value)
+            self.set_next_node(self.top_item)
             self.top_item = value
             self.size += 1
         else:
@@ -34,7 +34,7 @@ class Stack(Node):
     def is_empty(self):
         return self.size <= 0
 
-    def poo(self):
+    def pop(self):
         if not self.is_empty():
             item_to_remove = self.top_item
             top_item = self.get_link_node()
@@ -42,3 +42,25 @@ class Stack(Node):
             return item_to_remove
         else:
             print("Stack Empty")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.get_value()
+        else:
+            print("Stack Empty")
+
+Pizza_Stack = Stack(4,5)
+
+print(Pizza_Stack.has_space())
+
+print(Pizza_Stack.is_empty())
+
+Pizza_Stack.push("Veggie Supreme")
+Pizza_Stack.push("Country Feast")
+Pizza_Stack.push("Spiced Paneer")
+
+print(Pizza_Stack.pop())
+
+Pizza_Stack.push("Chicken Sausage N Tikka")
+
+print(Pizza_Stack.peek())
