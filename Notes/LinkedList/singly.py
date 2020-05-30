031e0049e3eba41c6a8a5d95cc6e396aedda7b18
anubhav11803451 <anubhav.11803451@lpu.in>
2020-05-30 15:08:12 +0530

    Update singly.py

--- Notes/LinkedList/singly.py
+++ Notes/LinkedList/singly.py
@@ -8,11 +8,17 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def Print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
-            return 
+            return
         last_node = self.head
         while last_node.next:
             last_node = last_node.next
