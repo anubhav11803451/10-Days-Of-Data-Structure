4c93b9e6451df23c335c4ac16b883b6809ae0361
anubhav11803451 <anubhav.11803451@lpu.in>
2020-05-30 21:43:14 +0530

    Update singlyLL.py

--- Tasks/Day_3/singlyLL.py
+++ Tasks/Day_3/singlyLL.py
@@ -12,6 +12,20 @@ class Node:
     def set_next_node(self, next_node):
         self.next_node = next_node
 
+class LinkedList:
+    def __init__(self, value = None):
+        new_head_node = Node(value)
+        self.head_node = new_head_node
+
+    def get_head_node(self):
+        return self.head_node
+
+    def insert_beginning(self, new_value):
+        new_node = Node(new_value)
+        new_node.next_node = self.head_node
+        self.head_node = new_node
+
+    def stringify_list()
 
 my_Node = Node(44)
 
