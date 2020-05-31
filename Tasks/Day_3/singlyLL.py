a0d8e9bf58d602e05c6e7d823450fef493162781
anubhav11803451 <anubhav.11803451@lpu.in>
2020-05-31 10:24:55 +0530

    Update singlyLL.py

--- Tasks/Day_3/singlyLL.py
+++ Tasks/Day_3/singlyLL.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self, value, next_node = None):
+    def __init__(self, value, next_node=None):
         self.value = value
         self.next_node = next_node
-    
+
     def get_value(self):
         return self.value
 
@@ -12,10 +12,10 @@ class Node:
     def set_next_node(self, next_node):
         self.next_node = next_node
 
+
 class LinkedList:
-    def __init__(self, value = None):
-        new_head_node = Node(value)
-        self.head_node = new_head_node
+    def __init__(self, value=None):
+        self.head_node = Node(value)
 
     def get_head_node(self):
         return self.head_node
@@ -25,9 +25,26 @@ class LinkedList:
         new_node.next_node = self.head_node
         self.head_node = new_node
 
-    def stringify_list():
-        
+    def stringify_list(self):
+        string = ""
+        start = self.head_node
+        while start:
+            string += str(start.get_value())+"\n"
+            start = start.next_node
+        return string
 
-my_Node = Node(44)
+    def remove_node(self, value_to_remove):
+        current_node = self.head_node
+        if current_node == value_to_remove:
+            self.head_node = current_node.next_node
+        else:
+            while current_node:
+                if (current_node.get_next_node()).get_next_node() == value_to_remove:
+                    current_node = current_node.next_node.get_next_node()
+                    current_node.next_node = None
+                current_node = current_node.next_node
 
-print(my_Node.get_value())
+
+myNode = Node(44)
+
+print(myNode.get_value())
