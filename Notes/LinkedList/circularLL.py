826017c9515f9f5a62b6e977e24991481b9ad3eb
anubhav11803451 <anubhav.11803451@lpu.in>
2020-05-31 20:49:25 +0530

    Update circularLL.py

--- Notes/LinkedList/circularLL.py
+++ Notes/LinkedList/circularLL.py
@@ -3,10 +3,11 @@ class Node:
         self.data = data
         self.next = None
 
+
 class CircularLinkedList:
     def __init__(self):
         self.head = None
-    
+
     def prepend(self, data):
         new_node = Node(data)
         cur = self.head
@@ -39,11 +40,39 @@ class CircularLinkedList:
             if cur == self.head:
                 break
 
+    def remove(self, key):
+        if self.head:
+            if self.head.data == key:
+                cur = self.head 
+                while cur.next != self.head:
+                    cur = cur.next 
+                if self.head == self.head.next:
+                    self.head = None
+                else:
+                    cur.next = self.head.next
+                    self.head = self.head.next
+            else:
+                cur = self.head 
+                prev = None 
+                while cur.next != self.head:
+                    prev = cur 
+                    cur = cur.next
+                    if cur.data == key:
+                        prev.next = cur.next 
+                        cur = cur.next
+    
+    
+
 CLlist = CircularLinkedList()
 
 CLlist.append("C")
 CLlist.append("D")
 CLlist.prepend("B")
 CLlist.prepend("A")
+
+CLlist.print_list()
+
+print("Circular LinkedList after removing:")
+CLlist.remove("A")
 
 CLlist.print_list()
