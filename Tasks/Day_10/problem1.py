b4386f2a1c622fa2e39d2828011dece89884230f
anubhav11803451 <anubhav.11803451@lpu.in>
2020-06-07 11:36:46 +0530

    Create problem1.py

---
+++ Tasks/Day_10/problem1.py
@@ -0,0 +1,13 @@
+x = 50
+y = 1000
+
+
+def recursive_multiply(x, y):
+    if x < y:
+        return recursive_multiply(y, x)
+    if y == 0:
+        return 0
+    return x + recursive_multiply(x, y-1)
+
+
+print(recursive_multiply(x, y))
