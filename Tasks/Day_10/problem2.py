ba2ecedb88fc19783581f4fa1263f3846d2c967b
anubhav11803451 <anubhav.11803451@lpu.in>
2020-06-07 11:36:49 +0530

    Create problem2.py

---
+++ Tasks/Day_10/problem2.py
@@ -0,0 +1,24 @@
+vowels = "aeiou"
+
+input_str = "Welcome to Educative!"
+
+
+def iterative_count_consonants(input_str):
+    consonant_count = 0
+    for i in range(len(input_str)):
+        if input_str[i].lower() not in vowels and input_str[i].isalpha():
+            consonant_count += 1
+    return consonant_count
+
+
+def recursive_count_consonants(input_str):
+    if input_str == '':
+        return 0
+
+    if input_str[0].lower() not in vowels and input_str[0].isalpha():
+        return 1 + recursive_count_consonants(input_str[1:])
+    else:
+        return recursive_count_consonants(input_str[1:])
+
+
+print(iterative_count_consonants(input_str))
